d6f9d8a3951f75109ba4a72a3671f972e9aff563
Serena Toon <stoo718@aucklanduni.ac.nz>
2017-11-23 15:45:13 +1300

    bot framework

--- bot/app.js
+++ bot/app.js
@@ -12,8 +12,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: "53c738ba-56ec-421c-aa14-dac683e30553",
+    appPassword: "spfndWU4032!upJSRSN8~~_"
 });
 
 // Listen for messages from users 
