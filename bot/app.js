7aa965a30efc6774aac9b7d7d81a73214802712c
Serena Toon <stoo718@aucklanduni.ac.nz>
2017-11-23 16:01:40 +1300

    update password

--- bot/app.js
+++ bot/app.js
@@ -13,7 +13,7 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
     appId: "53c738ba-56ec-421c-aa14-dac683e30553",
-    appPassword: "spfndWU4032!upJSRSN8~~_"
+    appPassword: "kysmdeHV89}~eRBACZ567#:"
 });
 
 // Listen for messages from users 
