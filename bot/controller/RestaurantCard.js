73136fd677c5418a2e4a06deeca00d8dfa00efd5
Serena Toon <stoo718@aucklanduni.ac.nz>
2017-11-23 13:27:38 +1300

    use own yelp api key

--- bot/controller/RestaurantCard.js
+++ bot/controller/RestaurantCard.js
@@ -4,7 +4,7 @@ var builder = require('botbuilder');
 //Calls 'getYelpData' in RestClient.js with 'displayRestaurantCards' as callback to get list of restaurant information
 exports.displayRestaurantCards = function getRestaurantData(foodName, location, session){
     var url ='https://api.yelp.com/v3/businesses/search?term='+foodName+'&location='+location + '&limit=5';
-    var auth ='cO92idzWqWjpOsV8RdAoB2DZl2GW8OE8pvoTlOjNNI0gbA2J7xXuiAPtLAYCkPCKR-dIXG3ePsSI4ngt8WRNQ4q4RlKMdXyvJr6r4_L3kndI5wpznLN6WUrPmgDYWXYx';
+    var auth ='YhfX4YUtXddq_Ba8nTvhwpgTflRxQV5m0gxa5iGWAN3q6Th631PpxPH5wKUHQOoqyjh5NUHJlK1r_yWO2UWod9SjMhqPOSkY8_xdn__AmVJjrUcZCH2jr_hlrxQWWnYx';
     rest.getYelpData(url,auth,session,displayRestaurantCards);
 }
 
