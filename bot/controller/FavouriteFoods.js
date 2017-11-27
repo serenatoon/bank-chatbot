2d094fc978b25c1602459532b4a12768d9962fc3
Serena Toon <stoo718@aucklanduni.ac.nz>
2017-11-27 13:36:57 +1300

    revert to customvision

---
+++ bot/controller/FavouriteFoods.js
@@ -0,0 +1,62 @@
+var rest = require('../API/RestClient');
+
+exports.displayFavouriteFood = function getFavouriteFood(session, username){
+    var url = 'https://fooodbot.azurewebsites.net/tables/FoodBot';
+    rest.getFavouriteFood(url, session, username, handleFavouriteFoodResponse)
+};
+
+function handleFavouriteFoodResponse(message, session, username) {
+	//session.send("handling"); 
+    var favouriteFoodResponse = JSON.parse(message);
+    var allFoods = [];
+    for (var index in favouriteFoodResponse) {
+        var usernameReceived = favouriteFoodResponse[index].username;
+        var favouriteFood = favouriteFoodResponse[index].favouriteFood;
+
+        //Convert to lower case whilst doing comparison to ensure the user can type whatever they like
+        if (username.toLowerCase() === usernameReceived.toLowerCase()) {
+            //Add a comma after all favourite foods unless last one
+            if(favouriteFoodResponse.length - 1) {
+                allFoods.push(favouriteFood);
+            }
+            else {
+                allFoods.push(favouriteFood + ', ');
+            }
+        }        
+    }
+    
+    // Print all favourite foods for the user that is currently logged in
+    session.send("%s, your favourite foods are: %s", username, allFoods);                
+    
+}
+
+exports.sendFavouriteFood = function postFavouriteFood(session, username, favouriteFood){
+    var url = 'https://fooodbot.azurewebsites.net/tables/FoodBot';
+    rest.postFavouriteFood(url, username, favouriteFood);
+};
+
+exports.deleteFavouriteFood = function deleteFavouriteFood(session,username,favouriteFood){
+    var url  = 'https://fooodbot.azurewebsites.net/tables/FoodBot';
+
+
+    rest.getFavouriteFood(url,session, username,function(message,session,username){
+     var   allFoods = JSON.parse(message);
+
+        for(var i in allFoods) {
+
+            if (allFoods[i].favouriteFood === favouriteFood && allFoods[i].username === username) {
+
+                console.log(allFoods[i]);
+
+                rest.deleteFavouriteFood(url,session,username,favouriteFood, allFoods[i].id ,handleDeletedFoodResponse)
+
+            }
+        }
+
+
+    });
+};
+
+function handleDeletedFoodResponse(message, session, username, favouriteFood) {
+	
+}
