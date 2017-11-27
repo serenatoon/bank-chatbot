bd619d5113baaabff965aa164c49a58442563c77
Serena Toon <stoo718@aucklanduni.ac.nz>
2017-11-28 12:04:08 +1300

    botframework credentials

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ server.listen(process.env.port || process.env.PORT || 80, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: "dd22faf6-74a6-44f7-9036-d7511caa03bc",
+    appPassword: "zoxgQP872*#+hjlLDKUX25)"
 });
 
 // Listen for messages from users 
