de1a67d063969332424b9c59de7044434ecc040f
Serena Toon <stoo718@aucklanduni.ac.nz>
2017-11-29 12:53:26 +1300

    gets datetime dynamically, shows on basic card

--- controller/StockCard.js
+++ controller/StockCard.js
@@ -8,17 +8,31 @@ exports.displayStockCards = function getStockData(symbol, session){
     rest.getStockData(url, session,symbol, displayStockCards);
 }
 
+/*  Get yesterday's date as a YYYY-MM-DD string
+    Source: https://stackoverflow.com/questions/23593052/format-javascript-date-to-yyyy-mm-dd */
+function getDate() {
+    var yesterdate = new Date(Date.now()-86400000); // get yesterday's date as a Date object
+    month = '' + (yesterdate.getMonth() + 1),
+    day = '' + yesterdate.getDate(),
+    year = yesterdate.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+
 function displayStockCards(message, symbol,session){
     //Parses JSON
     var response = JSON.parse(message);
-    var index = 'Time Series (15min)';
-    var time = '2017-11-28 16:00:00';
+    var time_series = 'Time Series (15min)';
+    //var time = '2017-11-28 16:00:00';
+    var time = getDate() + ' 16:00:00';
     var close = '4. close';
     //Adds first 5 Stock information (i.e calories, energy) onto list
-    var Stock = response;
-    //console.log(Object.keys(Stock));
-    console.log(response[index][time][close]);
-    //console.log(Object.keys(response[index]));
+
+    console.log(response[time_series][time][close]);
+
     // var StockItems = [];
     // for(var i = 0; i < 5; i++){
     //     var StockItem = {};
@@ -27,50 +41,50 @@ function displayStockCards(message, symbol,session){
     //     StockItems.push(StockItem);
     // }
 
-    // //Displays Stock adaptive cards in chat box 
-    // session.send(new builder.Message(session).addAttachment({
-    //     contentType: "application/vnd.microsoft.card.adaptive",
-    //     content: {
-    //         "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
-    //         "type": "AdaptiveCard",
-    //         "version": "0.5",
-    //         "body": [
-    //             {
-    //                 "type": "Container",
-    //                 "items": [
-    //                     {
-    //                         "type": "TextBlock",
-    //                         "text": symbol.charAt(0).toUpperCase() + symbol.slice(1),
-    //                         "size": "large"
-    //                     },
-    //                     {
-    //                         "type": "TextBlock",
-    //                         "text": "Stockal Information"
-    //                     }
-    //                 ]
-    //             },
-    //             {
-    //                 "type": "Container",
-    //                 "spacing": "none",
-    //                 "items": [
-    //                     {
-    //                         "type": "ColumnSet",
-    //                         "columns": [
-    //                             {
-    //                                 "type": "Column",
-    //                                 "width": "auto",
-    //                                 "items": [
-    //                                     {
-    //                                         "type": "FactSet",
-    //                                         "facts": StockItems
-    //                                     }
-    //                                 ]
-    //                             }
-    //                         ]
-    //                     }
-    //                 ]
-    //             }
-    //         ]
-    //     }
-    // }));
+    //Displays Stock adaptive cards in chat box 
+    session.send(new builder.Message(session).addAttachment({
+        contentType: "application/vnd.microsoft.card.adaptive",
+        content: {
+            "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+            "type": "AdaptiveCard",
+            "version": "0.5",
+            "body": [
+                {
+                    "type": "Container",
+                    "items": [
+                        {
+                            "type": "TextBlock",
+                            "text": symbol.toUpperCase(),
+                            "size": "large"
+                        },
+                        {
+                            "type": "TextBlock",
+                            "text": '$' + response[time_series][time][close]
+                        }
+                    ]
+                },
+                {
+                    "type": "Container",
+                    "spacing": "none",
+                    "items": [
+                        {
+                            "type": "ColumnSet",
+                            "columns": [
+                                {
+                                    "type": "Column",
+                                    "width": "auto",
+                                    "items": [
+                                        {
+                                            "type": "FactSet",
+                                            "facts": " sddfsdf"
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    }));
 }
