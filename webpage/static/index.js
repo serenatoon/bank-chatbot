405dc24445323e7fc3f73602e9391bdb4f8f91dd
Serena Toon <stoo718@aucklanduni.ac.nz>
2017-11-30 00:10:51 +1300

    documentation of functionality on webpage

--- webpage/static/index.js
+++ webpage/static/index.js
@@ -1,8 +1,8 @@
 // change text when link is clicked 
 // adapted from https://stackoverflow.com/questions/16407315/changing-content-in-a-div-when-a-link-is-clicked
-$(document).ready(function() {
+
     $('.question1').click(function(){
-        $('.new_member_box_display').text($('.answer1 ').text());
+        $('.new_member_box_display').text($('.answer1').text());
     });
 
     // $('.question2').click(function(){
@@ -16,4 +16,7 @@ $(document).ready(function() {
     // $('.question4').click(function(){
     //  $('.new_member_box_display').text($('.answer4').text());
     // });
+
+$( "p" ).click(function() {
+  $( this ).slideUp();
 });
