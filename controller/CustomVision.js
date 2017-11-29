2c89b83cc32a2ac3a02900b141296c02f98516aa
Serena Toon <stoo718@aucklanduni.ac.nz>
2017-11-29 20:44:51 +1300

    attempt to file upload??

--- controller/CustomVision.js
+++ controller/CustomVision.js
@@ -1,14 +1,15 @@
 var request = require('request'); //node module for http post requests
 
 exports.retreiveMessage = function (session){
+    console.log(session.message.attachments[0].contentType);
     request.post({
-        url: 'https://southcentralus.api.cognitive.microsoft.com/customvision/v1.0/Prediction/91c4f47c-1556-49d0-bf0f-3336ca58846e/url?iterationId=ed03d100-e286-4c72-8700-40470e46e1b0',
+        url: 'https://southcentralus.api.cognitive.microsoft.com/customvision/v1.0/Prediction/91c4f47c-1556-49d0-bf0f-3336ca58846e/image?iterationId=ed03d100-e286-4c72-8700-40470e46e1b0',
         json: true,
         headers: {
-            'Content-Type': 'application/json',
+            'Content-Type': 'application/octet-stream',
             'Prediction-Key': 'bbc58f8350f14da79ad614faa33b7d08'
         },
-        body: { 'Url': session.message.text }
+        body: session.message.attachments[0]
     }, function(error, response, body){
         console.log(validResponse(body));
         session.send(validResponse(body));
